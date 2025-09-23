b5b66de717b6a2adb1064450c234d48a545c0796
Scott Wilson <scott.wilson@workhorse.com>
2025-09-23 14:23:15 -0400

    rover delivery smart rtl

--- server.js
+++ server.js
@@ -25,12 +25,20 @@ const rover = {
   commandComponent: 1,
   targetSystem: 1,
   targetComponent: 0,
+  dateFormat: require('dateformat'),
+  fs: require('fs'),
+  net: require('net'),
   SerialPort: require("serialport").SerialPort,
-  mavlink: require("./lib/mavlink.js"),
+  mavlink: require("./lib/pixhawk/mavlink.js"),
   init_logs: require("./lib/logging/init_logs.js"),
 	create_logs: require("./lib/logging/create_logs.js"),
 	logging: require("./lib/logging/logging.js"),
-  net: require('net'),
+  update_serialports: require('./lib/serial/update_serialports.js'),
+  request_data_stream: require('./lib/pixhawk/request_data_stream.js'),
+  mavlink_messages: require('./lib/pixhawk/mavlink_messages.js'),
+  send_pixhawk_command: require('./lib/pixhawk/send_pixhawk_command.js'),
+  pixhawk_message_handler: require('./lib/pixhawk/pixhawk_message_handler.js'),
+  set_flight_mode: require('./lib/pixhawk/set_flight_mode.js'),
   GPS: require("gps"),
   angles: require("angles"),
   bufferpack: require("bufferpack"),
@@ -44,6 +52,106 @@ const rover = {
 		robotkit: null
 
 	},
+  	logs: {
+		count: 1
+	},
+  robot_data: {
+		robot_latitude: 0,
+		robot_longitude: 0,
+		LOCAL_POSITION_NED: {},
+		GLOBAL_POSITION_INT: {
+			lat: 0,
+			lon: 0
+		},
+		HEARTBEAT: {},
+		SYS_STATUS: {},
+		STATUSTEXT: {},
+		ATTITUDE: {},
+		VFR_HUD: {},
+		GLOBAL_POSITION_INT: {
+			lat: 0,
+			lon: 0
+		},
+		SERVO_OUTPUT_RAW: {},
+		LOCAL_POSITION_NED: {},
+		ATTITUDE_QUATERNION: {},
+		HIGHRES_IMU: {},
+		GPS_RAW_INT: {},
+		PING: {},
+		SYSTEM_TIME: {},
+		RANGEFINDER: {},
+		MISSION_CURRENT: {},
+		COMMAND_ACK: {},
+		PARAM_ACK: {},
+		MISSION_ACK: {},
+		PARAM_VALUE: {},
+		DISTANCE_SENSOR: {}
+	},
+  	flight_data: {
+		mission_step: null,
+		launch_location: {},
+		land_location: {},
+		current_location: {},
+		horizontal_distance_m: 0,
+		vertical_distance_m: 0,
+		inflight: 0,
+		flight_type: null,
+		manual_intervention: null,
+		control_type: null,
+		robot_help: null,
+		help_reason: null,
+		robot_modem_signal_strength: null,
+		robot_flight_mode: null,
+		robot_alert: null,
+		robot_delivery_state: null,
+		robot_claw_state: null,
+		robot_dropoff_status: null,
+		travel_speed: null,
+		mav_state: null,
+		robot_base_mode: null,
+		package_delivered: null,
+		bowl_distance_m: null,
+		land_robot: null,
+        launch_command_received: null,
+        recall_cammand_received: null
+
+	},
+  	FlightModes: {
+		0: 'Stabilize',
+		1: 'Acro',
+		2: 'AltHold',
+		3: 'Auto',
+		4: 'Guided',
+		5: 'Loiter',
+		6: 'RTL',
+		7: 'Circle',
+		9: 'Land',
+		11: 'Drift',
+		13: 'Sport',
+		14: 'Flip',
+		15: 'AutoTune',
+		16: 'PosHold',
+		17: 'Brake',
+		18: 'THROW',
+		19: 'AVOID_ADSB',
+		20: 'GUIDED_NOGPS',
+		21: 'SMART_RTL',
+		22: 'FLOWHOLD',
+		23: 'FOLLOW'
+	},
+  	altitude: {
+		take_off_msl_alt_meters: 0,
+		rangefinder_alt_meters: 0,
+		relative_alt_meters: 0,
+		msl_alt_meters: 0,
+		updating_travel_alt: false
+	},
+  	mission: {
+		current_mission_seq: 0,
+		mission_count: 0,
+
+	},
+  mav_version: 2,
 };
 
 //Ports: define...................................
@@ -62,16 +170,9 @@ function update_serialports(show_ports) {
   });
 }
 
-//Connect on load.........................................................
 
-update_serialports(true);
-
-setInterval(function () {
-  //Retry connection...........
-  update_serialports(false);
-}, 5000);
-
-
+//Logs: Create..............
+rover.init_logs(rover);
 
 
 
