a073df8488f916ff314f49094f68c57d9bae28c4
Scott Wilson <swilson123@gmail.com>
2025-11-19 15:22:32 -0500

    rover throttle update

--- server.js
+++ server.js
@@ -238,6 +238,7 @@ const rover = {
     create_waveshare_message: require("./lib/waveshare/create_waveshare_message"),
     radio_commands: require("./lib/radio_controller/radio_commands"),
     move_rover: require("./lib/waveshare/move_rover"),
+    servo_send_command: require("./lib/servos/servo_send_command")
 
 };
 
@@ -270,15 +271,15 @@ rover.connect_to_waveshare(rover);
 
 
 // command constants from Waveshare example (ddsm_example/json_cmd.h)
-const CMD_DDSM_CTRL = 10010;        // speed/current/position control
-const CMD_DDSM_CHANGE_ID = 10011;   // change motor ID
-const CMD_CHANGE_MODE = 10012;      // change mode
-const CMD_DDSM_ID_CHECK = 10031;    // query motor ID (only one motor connected)
-const CMD_DDSM_INFO = 10032;        // get info for a motor
-const CMD_HEARTBEAT_TIME = 11001;   // set heartbeat time
+// const CMD_DDSM_CTRL = 10010;        // speed/current/position control
+// const CMD_DDSM_CHANGE_ID = 10011;   // change motor ID
+// const CMD_CHANGE_MODE = 10012;      // change mode
+// const CMD_DDSM_ID_CHECK = 10031;    // query motor ID (only one motor connected)
+// const CMD_DDSM_INFO = 10032;        // get info for a motor
+// const CMD_HEARTBEAT_TIME = 11001;   // set heartbeat time
 
-const motor_id = 4
-const motor_speed_cmd = 200;
+// const motor_id = 4
+// const motor_speed_cmd = 200;
 
 
 // send change ID (example: set motor with physical connection to ID 1 -> change to 2)
@@ -294,40 +295,30 @@ const motor_speed_cmd = 200;
 //     }
 // }, 1000);
 
-setTimeout(() => {
-    if (rover.waveshare.connected) {
-        var message = { "T": CMD_CHANGE_MODE, "id": motor_id, "mode": 2 };
-        rover.create_waveshare_message(rover, message);
-    } else {
-        console.log("Waveshare not connected");
-    }
-}, 2000);
+// setTimeout(() => {
+//     if (rover.waveshare.connected) {
+//         var message = { "T": CMD_CHANGE_MODE, "id": motor_id, "mode": 2 };
+//         rover.create_waveshare_message(rover, message);
+//     } else {
+//         console.log("Waveshare not connected");
+//     }
+// }, 2000);
 
-// set heartbeat time (-1 disables automatic stop)
-setTimeout(() => {
-    if (rover.waveshare.connected) {
-        var message = { "T": CMD_HEARTBEAT_TIME, "time": 2000 };
-        rover.create_waveshare_message(rover, message);
-    } else {
-        console.log("Waveshare not connected");
-    }
-}, 3000);
-
-
+// // set heartbeat time (-1 disables automatic stop)
+// setTimeout(() => {
+//     if (rover.waveshare.connected) {
+//         var message = { "T": CMD_HEARTBEAT_TIME, "time": 2000 };
+//         rover.create_waveshare_message(rover, message);
+//     } else {
+//         console.log("Waveshare not connected");
+//     }
+// }, 3000);
 
 
 
 
 
-// send control command (speed/current/position depending on mode)
-setTimeout(() => {
-    if (rover.waveshare.connected) {
-        var message = { "T": CMD_DDSM_CTRL, "id": motor_id, "cmd": motor_speed_cmd, "act": 3 };
-        rover.create_waveshare_message(rover, message);
-    } else {
-        console.log("Waveshare not connected");
-    }
-}, 4000);
+
 
 
 
